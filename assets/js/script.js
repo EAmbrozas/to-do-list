af719924fd9c77c65a557792be1f99de9f5aae8e
EAmbrozas <ernestasambrozas@gmail.com>
2022-12-03 11:53:02 +0000

    Add selectors in JavaScript file

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//selectors
+const input = document.querySelector('#input');
+const addBtn = document.querySelector('.add_Btn');
+const listItems = document.querySelector('.list_items');
+
+//Event Listeners
+
+//Functions
