2ad77e4f0c090f7ba6fd8fe95f373e5fbb4e4e21
EAmbrozas <ernestasambrozas@gmail.com>
2022-12-03 14:20:14 +0000

    Add remove button for each to do list item and declare event listener with a function that will remove the list item when the remove button is clicked on to

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,24 @@ const listItems = document.querySelector('#list_items');
 //Event Listeners
 addBtn.addEventListener("click", addTodo);
 
-
 //Functions
 
 /**Adds to do list item in list items ul*/
-// function to ad list item
+// Function to add list item
 function addTodo(event) {
     const todoItem = document.createElement('li');
     todoItem.innerText = input.value;
     listItems.appendChild(todoItem);
+
+    // Appends remove button for each list item
+    const removeBtn = document.createElement('button');
+    removeBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    removeBtn.classList.add('remove_Btn');
+    todoItem.appendChild(removeBtn);
+
+    // Removes list item when the remove button is clicken on to
+    removeBtn.addEventListener("click", removeTodo);
+    function removeTodo(event) {
+        todoItem.remove();
+    }
 }
