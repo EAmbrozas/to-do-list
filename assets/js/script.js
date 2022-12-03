80d303c106c1adad0e5de3477b7aef65c920fdc5
EAmbrozas <ernestasambrozas@gmail.com>
2022-12-03 13:28:18 +0000

    Add event listenewith todo function that will add a new to do list item when the add button is clicked on to

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,18 @@
 //selectors
 const input = document.querySelector('#input');
 const addBtn = document.querySelector('.add_Btn');
-const listItems = document.querySelector('.list_items');
+const listItems = document.querySelector('#list_items');
 
 //Event Listeners
+addBtn.addEventListener("click", addTodo);
+
 
 //Functions
+
+/**Adds to do list item in list items ul*/
+// function to ad list item
+function addTodo(event) {
+    const todoItem = document.createElement('li');
+    todoItem.innerText = input.value;
+    listItems.appendChild(todoItem);
+}
